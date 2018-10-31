268b6418d1b259ab26002062e9bd635dc8a52ba4
anatolikarman <38259294+anatolikarman@users.noreply.github.com>
2018-10-31 23:00:38 +0200

    Add files via upload

---
+++ homework2.java
@@ -0,0 +1,126 @@
+package com.htp;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class homework2 {
+
+    public static void main(String[] args) {
+        int[] iArray = {100, 200, 600, 400, 500};
+        String[] abc = {"B", "A", "C"};
+
+
+        //Task1
+//        Arrays.sort(iArray);
+//        Arrays.sort(abc);
+//
+//        for (int i = 0; i < iArray.length; i++) {
+//            System.out.println(iArray[i]);
+//        }
+//        for (int j = 0; j < abc.length;j++) {
+//            System.out.println(abc[j]);
+//        }
+
+        //Task2
+//        int sum = 0;
+//        for (int i = 0; i<iArray.length; i++){
+//            sum = sum + iArray[i];
+//        }
+//        System.out.println(sum);
+//}
+
+        //Task3
+//        int a = 5;
+//        int b = 10;
+//        for (int i = 0; i< a; i++){
+//            for (int j=0; j <b; j++){
+//                System.out.print("-");
+//            }
+//            System.out.println();
+//        }
+
+        //Task4
+//        int sum = 0;
+//        for (int i = 0; i < iArray.length; i++) {
+//            sum = sum + iArray[i];
+//        }
+//        int avrg = sum / iArray.length;
+//        System.out.println(avrg);
+
+        //Task5
+//       int a = 500;
+//       for (int i = 0; i < iArray.length; i++) {
+//           if (iArray[i] == a) {
+//               System.out.println("Значение есть в массиве");
+//               break;
+//           } else {
+//               if (i == iArray.length - 1) {
+//                   System.out.println("Такого значения нет в массиве");
+//               }
+//           }
+
+        //Task6
+//        int a = 4000;
+//
+//        for (int i = 0; i < iArray.length; i++) {
+//            if (iArray[i] == a) {
+//                System.out.println(i);
+//                break;
+//            } else {
+//                if (i == iArray.length - 1) {
+//                    System.out.println("Такого значения нет в массиве");
+//                }
+//            }
+//
+//        }
+
+
+        //Task7
+//        int a = 500;
+//        for (int i = 0; i < iArray.length; i++) {
+//            if (iArray[i] == a) {
+//                iArray[i] = 0;
+//                System.out.println(a + " удалено из массива");
+//                break;
+//            } else {
+//                if (i == iArray.length - 1) {
+//                    System.out.println("Такого значения нет в массиве");
+//                }
+//            }
+
+
+        //Task9
+//        int element = 1000000;
+//        int b = iArray.length - 1;
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Введите индекс от 0 до " + b);
+//        int index = input.nextInt();
+//        System.out.println("Введите значение");
+//        int value = input.nextInt();
+//        iArray[index] = value;
+//        for (int i = 0; i < iArray.length; i++) {
+//            System.out.println(iArray[i]);
+//        }
+
+
+        //Task10
+//        Arrays.sort(iArray);
+//        int max = iArray[iArray.length-1];
+//        int min = iArray [0];
+//        System.out.println("Максимальное значение равно " + max);
+//        System.out.println("Минимальное значение равно " + min);
+
+        //Task17
+//        Arrays.sort(iArray);
+//        int second = iArray[iArray.length - 2];
+//        System.out.println("Второй по величине элемент " + second);
+
+
+    }
+}
+
+
+
+
+
