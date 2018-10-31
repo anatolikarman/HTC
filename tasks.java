a152ff63871b9393eb370b7365492b00ca496233
anatolikarman <38259294+anatolikarman@users.noreply.github.com>
2018-10-31 20:24:46 +0200

    Add files via upload
    Домашняя работа по первому занятию

---
+++ tasks.java
@@ -0,0 +1,148 @@
+package com.htp;
+
+import java.util.Scanner;
+
+public class tasks {
+    public static void main(String[] args) {
+
+//       Exercise1
+//        int mass1 = 82;
+//        double massOnMoon = 82 * 0.16;
+//        System.out.println(massOnMoon);
+
+
+//        Exercise4
+//int a = 100;
+//int b = 0;
+//while (a>0){
+//    b=a%2;
+//    if (b==0){
+//        System.out.println(a);
+//    }
+//    a--;
+//}
+
+
+        // Exercise5
+//        int a = 15;
+//        int b = 0;
+//        while (a>0){
+//            b=a%2;
+//            if (b!=0){
+//                System.out.println(a);
+//            }
+//            a--;
+//
+//        }
+
+
+        //Exercise7
+//        int a = 20;
+//        int c =0;
+//
+//        for (a = 20; a <= 200;a++ ) {
+//           c = c + a;
+//            System.out.println(c);
+//        }
+
+
+        //Exercise9
+//        int a = 3;
+//        int b = 4;
+//        int c = 0;
+//        c = a;
+//        a = b;
+//        b = c;
+//        System.out.println(b + " " + a);
+
+
+        //Exercise10
+//        int a = 5;
+//        int b = 9;
+//        int c = a + b;
+//        System.out.println(c);
+
+
+        //Exercise12
+//        boolean a = true;
+//        boolean b = false;
+//        if (a) {
+//            System.out.println("Истина");
+//        } else {
+//            System.out.println("Ложь");
+//        }
+//        if (b) {
+//            System.out.println("Истина");
+//        } else {
+//            System.out.println("Ложь");
+//        }
+
+//       Exercise13
+//        String a = "I don't know";
+//        String b = " what I'm doing.";
+//        String c = a + b;
+//        System.out.println(c);
+
+//Exercise14
+//        int a = 9   ;
+//        int b = 8;
+//        if (a>b){
+//            a+=3;
+//        } else {
+//            b+=8;
+//        }
+//        System.out.println(a + " " + b);
+
+
+        // Exercise15
+//        int a = 1;
+//        int b = 100;
+
+//        for (;a<=b;a++){
+//            System.out.println(a);
+//        }
+
+//        while (a<=b){
+//            System.out.println(a);
+//            a++;
+//        }
+
+//        do {
+//            System.out.println(a);
+//            a++;
+//        }
+//        while(a<=b);
+
+
+
+
+
+        //Exercise16
+//        System.out.println("Min: ");
+//        Scanner min = new Scanner(System.in);
+//        int num1 = min.nextInt();
+//
+//        System.out.println("Max: ");
+//        Scanner max = new Scanner(System.in);
+//        int num2 = max.nextInt();
+//
+//        System.out.println("Step: ");
+//        Scanner step = new Scanner(System.in);
+//        int num3 = step.nextInt();
+//
+//        System.out.println(num1);
+//        for (; num1<num2;){
+//
+//            num1 = num1+ num3;
+//            if (num1>num2){
+//                break;}
+//            System.out.println(num1);
+
+
+    }
+
+
+}
+
+
+
